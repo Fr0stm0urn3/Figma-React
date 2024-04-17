c10b39bd070a0f37a4eaec49b4f77466e4d66fd6
Cifer0101 <l.kakabadze2020@gmail.com>
2024-04-17 17:42:14 +0400

    CTA

--- bytebites/src/components/CTA/CTA.jsx
+++ bytebites/src/components/CTA/CTA.jsx
@@ -1,5 +1,18 @@
+import styles from "./CTA.module.css"
+
 const CTA = () => {
-  return <div>CTA</div>
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h2>Ready To Experience Exceptional Hosting?</h2>
+        <p>
+          We provide the best hosting and domain services for all your needs with
+          unlimited support services
+        </p>
+        <button>Get Started Now</button>
+      </div>
+    </section>
+  )
 }
 
 export default CTA
